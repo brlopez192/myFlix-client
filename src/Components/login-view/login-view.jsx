c64469d083d54c4b2d30bb27acea65b0702c0f7f
brlopez192 <brlopez192@gmail.com>
2025-02-17 16:33:27 -0500

    removed handlesubmit

---
+++ src/Components/login-view/login-view.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export const LoginView = ({onLoggedIn}) => {
+    const [username, setUsername] = useState("");
+    const handleSubmit = (event) => {}
+    return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    );
+};
