9f734e40cd824927124033bcfc192e56d8fe1374
brlopez192 <brlopez192@gmail.com>
2025-02-17 16:33:50 -0500

    added handle-submit

--- src/Components/login-view/login-view.jsx
+++ src/Components/login-view/login-view.jsx
@@ -2,7 +2,36 @@ import React, { useState } from "react";
 
 export const LoginView = ({onLoggedIn}) => {
     const [username, setUsername] = useState("");
-    const handleSubmit = (event) => {}
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password
+          };
+      
+          fetch("https://movieflixapp-88791d8c1b4d.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("Login response: ", data);
+              if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
+                onLoggedIn(data.user, data.token);
+              } else {
+                alert("No such user");
+              }
+            })
+            .catch((e) => {
+              alert("Something went wrong");
+            });
+        };
     return (
     <form onSubmit={handleSubmit}>
       <label>
