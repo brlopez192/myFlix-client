f24723bd507e0034a1cb8601986e5e39060c1a65
brlopez192 <brlopez192@gmail.com>
2025-02-17 16:36:32 -0500

    minor adjustments

--- src/Components/MainView/main-view.jsx
+++ src/Components/MainView/main-view.jsx
@@ -1,39 +1,53 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-        id: 1,
-        title: "Inception",
-        description: "A skilled thief who enters the dreams of others to steal secres is given a chance to have his past crimes forgiven.",
-        image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg",
-        genre: ["Sci-Fi", "Thriller"],
-        director: "Christopher Nolan" 
-
-    },
-    {
-        id: 2,
-        title: "The Shawshank Redemption",
-        description: "A man wrongly convicted of murder forms an unlikely friendship and plans his escape from prison",
-        image: "https://m.media-amazon.com/images/I/717W9DCnyzL._AC_UF894,1000_QL80_.jpg",
-        genre: ["Drama"],
-        director: "Frank Darabont" 
-
-    },
-    {
-        id: 3,
-        title: "The Dark Knight",
-        description: "Batman faces off against the Joker, a criminal mastermind who seeks to plunge Gotham into chaos.",
-        image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg",
-        genre: ["Action", "Crime", "Drama"],
-        director: "Cristopher Nolan" 
-        
-    }
-  ]);
-
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movieflixapp-88791d8c1b4d.herokuapp.com/movies", {
+      headers: {Authorization: 'Bearer ${token}'}
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = movies.map((movie) => {
+          return {
+            id: movie.id,
+            title: movie.title,
+            Image: movie.image,
+            description: movie.description,
+            director: movie.director,
+            Genre: movie.Genre,
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+    }, [token]);
+
+    if (!user) {
+      return (
+        <>
+          <LoginView onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }} />
+          or
+          <SignupView />
+        </>
+      );
+    }
 
   if (selectedMovie) {
     return (
@@ -47,6 +61,7 @@ export const MainView = () => {
 
   return (
     <div>
+      <button onClick={() => {setUser(null); setToken(null); localStorage.clear(); }}>LogOut</button>
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
